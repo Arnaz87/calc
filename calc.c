ef18d37c408696c2515526894961dd6ffe0de86b
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2017-11-03 17:39:28 -0400

    Funciones y variables

--- calc.c
+++ calc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 // Bigint en JS en el dominio público
 // https://github.com/Evgenus/BigInt/blob/master/src/BigInt.js
@@ -10,6 +11,13 @@
 
 // Números reales basado en la librería anterior
 // https://github.com/jtobey/javascript-bignum
+
+#define error(FMT,ARGS...) fprintf(stderr, FMT "\n", ## ARGS)
+
+
+
+
+//=== Types ===//
 
 enum Unit {
 	unitless,
@@ -26,26 +34,26 @@ enum Unit {
 
 typedef struct {
 	enum Unit unit;
-	int num;
-	int denom;
+	double n;
 } Value;
 
-void simplify (Value *value) {
-	int a = value->num;
-	int b = value->denom;
-	if (a<0) a *= -1;
-	if (b<0) b *= -1;
-	while (b != 0) {
-		int t = b;
-		b = a%b;
-		a = t;
-	}
-	int gcd = a;
-	value->num /= gcd;
-	value->denom /= gcd;
-	if (value->denom < 0)
-		value->num *= -1;
-}
+typedef struct {
+	char kind;
+	Value n;
+	char *str;
+	int args;
+} Item;
+
+typedef struct {
+	int len;
+	Item *items;
+} Expr;
+
+
+
+
+
+//=== Read ===///
 
 int argc = 0;
 char **argv = NULL;
@@ -92,15 +100,14 @@ void consumech () { ch = 0; }
 // Descartar lo que sea que haya y psar al siguiente caracter
 void nextch () { ch = getnextchchar(); }
 
-typedef struct {
+struct {
 	char kind; // w: word, v: value, f: function, \0: no token
 	Value value;
-	char str[16];
-} Token;
+	char *str;
+} token = { .kind = 0 };
 
-Token token = { .kind = 0 };
 
-void readhex (Value *value) {
+int readhex () {
 	nextch(); // el caracter leído es la 'x' de "0x"
 	int n = 0;
 	while (1) {
@@ -115,11 +122,10 @@ void readhex (Value *value) {
 		n = n*16+ d;
 		nextch();
 	}
-	value->num = n;
-	value->denom = 1;
+	return n;
 }
 
-void readbin (Value *value) {
+int readbin () {
 	nextch(); // el caracter leído es la 'b' de "0b"
 	int n = 0;
 	while (1) {
@@ -130,22 +136,19 @@ void readbin (Value *value) {
 		else break;
 		nextch();
 	}
-	value->num = n;
-	value->denom = 1;
+	return n;
 }
 
-void readvalue (Value *value) {
+double readvalue () {
 	int n = 0;
 	ensurech();
 	if (ch == '0') {
 		nextch();	
 		if (ch == 'x') {
-			readhex(value);
-			return;
+			return readhex();
 		}
 		if (ch == 'b') {
-			readbin(value);
-			return;
+			return readbin();
 		}
 	}
 	int div = 1;
@@ -168,41 +171,42 @@ point:
 		period--;
 	}
 	int denom = div*period;
-
-	value->num = n;
-	value->denom = denom;
+	return (double) n/denom;
 }
 
-void readtoken (Token *token) {
+void readtoken () {
 	ensurech();
 
 	// strip space
 	while (ch == ' ') nextch();
 
 	if ( iseof ) {
-		token->kind = 0;
+		token.kind = 0;
 		return;
 	}
 
 	if (ch >= '0' && ch <= '9') {
-		readvalue(&token->value);
-		token->kind = 'n';
-		token->value.unit = unitless;
+		token.value.n = readvalue();
+		token.kind = 'n';
+		token.value.unit = unitless;
 		return;
 	}
 
 	// Intentar leer un nombre
 	int count = 0;
+	char str[32];
 	while (ch >= 'a' && ch <= 'z') {
-		token->str[count++] = (char) ch;
+		str[count++] = (char) ch;
 		nextch();
 	}
 
 	if (count) {
-		token->str[count] = 0;
-		token->kind = 'w';
+		token.str = malloc(count+1);
+		strncpy(token.str, str, count);
+		token.str[count] = 0;
+		token.kind = 'w';
 	} else {
-		token->kind = (char) ch;
+		token.kind = (char) ch;
 		consumech();
 	}
 }
@@ -211,14 +215,14 @@ void ensuretk () { if (!token.kind) readtoken(&token); }
 void consumetk () { token.kind = 0; }
 void nexttk () { readtoken(&token); }
 
-char ops[16];
-int opc = 0;
-Value values[16];
-int valuec = 0;
 
-void execop ();
 
-void dovalue () {
+
+
+
+//=== Parse ===//
+
+Value parse_value () {
 	float sign = 1;
 	Value value;
 	ensuretk();
@@ -229,7 +233,7 @@ void dovalue () {
 	if (token.kind == 'n') {
 		value = token.value;
 		value.unit = unitless;
-		value.num *= sign;
+		value.n *= sign;
 		nexttk();
 	} else {
 		printf("Expected value\n");
@@ -237,30 +241,28 @@ void dovalue () {
 	}
 
 	if (token.kind == 'w') {
-		#define KW(W, U, N, D) \
+		#define KW(W, U, N) \
 			if (strcmp(token.str, W) == 0) {\
 				value.unit = U;\
-				value.num *= N;\
-				value.denom *= D;\
-				consumetk();\
+				value.n *= N;\
+				nexttk();\
 				goto end;\
 			}
-		KW("mm", distance, 1, 1000);
-		KW("cm", distance, 1, 100);
-		KW("m",  distance, 1, 1);
-		KW("km", distance, 1000, 1);
-		KW("g",  mass, 1, 1000);
-		KW("kg", mass, 1, 1);
-		KW("s",  time, 1, 1);
-		KW("min",time, 60, 1);
-		KW("h",  time, 3600, 1);
-		KW("day",time, 3600*24, 1);
-		KW("rad",angle, 355, 113);
+		KW("mm", distance, 0.0001);
+		KW("cm", distance, 0.001);
+		KW("m",  distance, 1);
+		KW("km", distance, 1000);
+		KW("g",  mass, 0.0001);
+		KW("kg", mass, 1);
+		KW("s",  time, 1);
+		KW("min",time, 60);
+		KW("h",  time, 3600);
+		KW("day",time, 3600*24);
+		KW("rad",angle, 3.14159268);
 		#undef KW
 	}
 end:
-	simplify(&value);
-	values[valuec++] = value; 
+	return value; 
 }
 
 int precedence (char op) {
@@ -268,6 +270,7 @@ int precedence (char op) {
 	switch (op) {
 	OP('(', 0)
 	OP(')', 0)
+	OP('f', 0)
 	OP('+', 1)
 	OP('-', 1)
 	OP('*', 2)
@@ -276,154 +279,244 @@ int precedence (char op) {
 	}
 }
 
-// Exponenciación racional
-void exp (Value *value, int e) {
-	if (e == 0) {
-		value->num = 1;
-		value->denom = 1;
-	}
+Expr parse_expr () {
+	Expr expr;
+	expr.items = 0;
+	expr.len = 0;
 
-	int neg = e < 0;
-	if (neg) e = -e;
+	Item items[100];
+	char ops[30];
+	struct {
+		char* str;
+		int args;
+	} calls[16];
+	int itemc=0, opc=0, callc=0;
 
-	while (--e > 0) {
-		value->num *= value->num;
-		value->denom *= value->denom;
-	}
+	#define pushop() items[itemc++].kind = ops[--opc]
 
-	if (neg) {
-		int temp = value->num;
-		value->num = value->denom;
-		value->denom = temp;
-	}
-}
-
-int pow (int b, int n) {
-	int r = 1;
-	while (--b > 0) {
-		r *= b;
-	}
-	return r;
-}
-
-void root (Value *value, int n) {
-	if (n == 1) return;
-	if (value->num < 0) {
-		value->num = 1;
-		value->denom = 0;
-		return;
-	}
-
-	int floored = value->num / value->denom;
-	int lastdiff = floored;
-	int guess = 1;
-	while (1) {
-		int thisguess = pow(guess, n);
-		int diff = thisguess - floored;
-		if (diff < lastdiff) {
-			lastdiff = diff;
-			guess++;
-		} else break;
-	}
-	value->num = guess;
-	value->denom = 1;
-}
-
-void execop () {
-	char op = ops[--opc];
-	Value b = values[--valuec];
-	Value a = values[--valuec];
-	Value r;
-	switch (op) {
-		case '+':
-			r.num = a.num*b.denom + a.denom*b.num;
-			r.denom = a.denom*b.denom;
-			break;
-		case '-':
-			r.num = a.num*b.denom - a.denom*b.num;
-			r.denom = a.denom*b.denom;
-			break;
-		case '*':
-			r.num = a.num * b.num;
-			r.denom = a.denom * b.denom;
-			break;
-		case '/':
-			r.num = a.num * b.denom;
-			r.denom = a.denom * b.num;
-			break;
-		case '^':
-			r = a;
-			exp(&r, b.num);
-			//root(&r, b.denom);
-			break;
-	}
-	simplify(&r);
-	values[valuec++] = r;
-}
-
-void dobinop () {
-	int match = 0;
-	ensuretk();
-	switch (token.kind) {
-		case '+':
-		case '-':
-		case '*':
-		case '/':
-		case '^':
-			match = 1;
-	}
-	if (!match) {
-		fprintf(stderr, "Expected operator %c\n", token.kind);
-		exit(1);
-	}
-	int p = precedence(token.kind);
-	while (p <= precedence(ops[opc-1]))
-		execop();
-	ops[opc++] = token.kind;
-	consumetk();
-}
-
-Value doline () {
-	// El '(' original...
+	// Para que los operadores no se salgan
 	ops[opc++] = '(';
-	
-	goto first;
+
+	ensuretk();
+
+	goto valstate;
 	while (token.kind) {
-		dobinop();
-	first:
-		ensuretk();
+		switch (token.kind) {
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+			case '^':
+				break;
+			default:
+				error("Expected operator");
+				return expr;
+		}
+
+		int p = precedence(token.kind);
+		while (p <= precedence(ops[opc-1]))
+			pushop();
+
+		ops[opc++] = token.kind;
+		nexttk();
+	valstate:
 		while (token.kind == '(') {
 			ops[opc++] = '(';
 			nexttk();
 		}
-		dovalue();
-		ensuretk();
+		
+		if (token.kind == 'n') {
+			items[itemc].n = parse_value();
+			items[itemc++].kind = 'n';
+		} else if (token.kind == 'w') {
+			nexttk();
+			if (token.kind == '(') {
+				nexttk();
+				calls[callc].str = token.str;
+				ops[opc++] = 'f';
+				if (token.kind == ')') { 
+					calls[callc++].args = 0;
+				} else {
+					calls[callc++].args = 1;
+					goto valstate;
+				}
+			} else {
+				items[itemc].str = token.str;
+				items[itemc++].kind = 'v';
+			}
+		}
+
 		while (token.kind == ')') {
-			while (ops[opc-1] != '(')
-				execop();
-			// Se eliminaron todos los operadores, queda el '('
+			while (precedence(ops[opc-1]) > 0) pushop();
+			if (opc < 2) {
+				error("Unmatched closing parenthesis");
+				return expr;
+			}
+			char op = ops[opc-1];
+			if (op == 'f') {
+				callc--;
+				items[itemc].kind = 'f';
+				items[itemc].str = calls[callc].str;
+				items[itemc].args = calls[callc].args;
+				itemc++;
+			}
 			opc--;
 			nexttk();
 		}
-		if (token.kind == '!') {
-			int i=0;
-			printf("ops:");
-			while (i < opc) printf(" %c", ops[i++]);
-			printf("\nvalues:");
-			i=0; while(i < valuec)
-				printf(" %d¬%d", values[i++].num, values[i].denom);
-			printf("\n");
+
+		if (token.kind == ',') {
+			while (precedence(ops[opc-1])) pushop();
+			if (ops[opc-1] != 'f') {
+				error("Misplaced comma");
+				return expr;
+			}
+			calls[callc-1].args++;
 			nexttk();
+			goto valstate;
 		}
+
 		if (token.kind == '\n' || token.kind == ';') {
 			consumetk();
 			break;
 		}
-	}
-	// Hasta que quede un operador: el '(' original!
-	while (opc > 1) execop();
+	}	
+	while (opc > 1) pushop();
+	#undef pushop
 
-	return values[valuec-1];
+	int bytes = itemc * sizeof(Item);
+	expr.items = malloc(bytes);
+	memcpy(expr.items, items, bytes);
+	expr.len = itemc;
+
+	return expr;
+}
+
+
+
+
+
+//=== Exec ===//
+
+Value V0 = (Value){unitless, 0};
+
+Value callstack[16];
+int stacklen = 0;
+
+Value my_log () {
+	if (stacklen < 1 || stacklen > 2) {
+		error("Argument mismatch");
+		return V0;
+	}
+	double x = callstack[0].n;
+	double b = stacklen>1? callstack[1].n : 10;
+	double r = log(x)/log(b);
+	return (Value){.n = r};
+}
+
+Value my_sin () {
+	if (stacklen != 1) {
+		error("Argument mismatch");
+		return V0;
+	}
+	return (Value){.n = sin(callstack[0].n)};
+}
+
+Value my_cos () {
+	if (stacklen != 1) {
+		error("Argument mismatch");
+		return V0;
+	}
+	return (Value){.n = cos(callstack[0].n)};
+}
+
+Value my_tan () {
+	if (stacklen != 1) {
+		error("Argument mismatch");
+		return V0;
+	}
+	return (Value){.n = tan(callstack[0].n)};
+}
+
+typedef Value (*ftype)();
+
+struct {
+	char *key;
+	ftype f;
+} funcs[] = {
+	{"log", my_log},
+	{"sin", my_sin},
+	{"cos", my_cos},
+	{"tan", my_tan}
+};
+
+int fcount = sizeof(funcs) / sizeof(*funcs);
+
+Value eval_expr (Expr expr) {
+	int pos = 0;
+	Value stack[30];
+
+	int i = 0;
+	while (i < expr.len) {
+		Item item = expr.items[i++];
+
+		if (item.kind == 'n') {
+			stack[pos++] = item.n;
+			continue;
+		}
+		if (item.kind == 'v') {
+			if (strcmp(item.str, "pi") == 0) {
+				stack[pos++].n = 3.14159265;
+				continue;
+			}
+			error("Unknown variable %s", item.str);
+			return V0;
+		}
+		if (item.kind == 'f') {
+			//printf("f: %s(%d)\n", item.str, item.args); return V0;
+			int i = 0;
+			ftype f = 0;
+			for(i=0; i<fcount; i++) {
+				if (strcmp(funcs[i].key, item.str) == 0)
+					f = funcs[i].f;
+			}
+			if (!f) {
+				error("Unknown function %s", item.str);
+				return V0;
+			}
+			stacklen = 0;
+			pos -= item.args;
+			while (stacklen < item.args)
+				callstack[stacklen++] = stack[pos + stacklen];
+			stack[pos++] = f();
+			stacklen = 0;
+			continue;
+		}
+		Value a = stack[--pos];
+		Value b = stack[--pos];
+		Value r;
+		switch (item.kind) {
+			case '+':
+				r.n = a.n + b.n;
+				break;
+			case '-':
+				r.n = a.n - b.n;
+				break;
+			case '*':
+				r.n = a.n * b.n;
+				break;
+			case '/':
+				r.n = a.n / b.n;
+				break;
+			case '^':
+				r.n = pow(a.n, b.n);
+				break;
+			default:
+				error("Unknown operator %c", item.kind);
+				return V0;
+		}
+		stack[pos++] = r;
+	}
+	return stack[0];
 }
 
 void main (int in_argc, char** in_argv) {
@@ -432,21 +525,17 @@ void main (int in_argc, char** in_argv) {
 
 	prepareargs();
 
+	printf("Calc\n");
+
 	readtoken(&token);
 	
-	/*while (token.kind) {
-		switch (token.kind) {
-			case 'w': printf("w: %s\n", token.str); break;
-			case 'f': printf("f: %s\n", token.str); break;
-			case 'n': printf("n: %f\n", token.value.v); break;
-			default: printf("char: %c\n", token.kind);
-		}
-		nexttk();
-	}*/
-	
 	while (token.kind) {
-		Value v = doline();
-		printf("%g\n", (double)v.num/v.denom );
+		Expr expr = parse_expr();
+		/*printf("expr: ");
+		int i;for(i=0;i<expr.len;i++) printf("%c ",expr.items[i]);
+		printf("\n");*/
+		Value v = eval_expr(expr);
+		printf("%g\n", v.n);
 		ensuretk();
 	}
 }
--- monitor.sh
+++ monitor.sh
@@ -1,1 +1,1 @@
-while (inotifywait -q -e close_write ./); do cc calc.c; done
+while (inotifywait -q -e close_write ./); do cc calc.c -lm && ./a.out; done
